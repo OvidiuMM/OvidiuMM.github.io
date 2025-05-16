bc658d5871686616c61eaf37fa41667728099848
Ovi <ovidiu5891@gmail.com>
2025-05-16 16:15:50 +0200

    fix: langauge switch

--- myscripts.js
+++ myscripts.js
@@ -1,25 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const element = document.getElementById('mySelect');
+
+    // set language
     const lanChange = () => {
         const selectedValue = element.value;
         if (selectedValue === 'en') {
             enDiv.style.display = "block";
             esDiv.style.display = "none";
-            document.documentElement.lang = "en";
         } else if (selectedValue === 'es') {
             enDiv.style.display = "none";
             esDiv.style.display = "block";
-            document.documentElement.lang = "es";
         }
-        //console.log('Selected:', selectedValue);
+        console.log('Selected:', selectedValue);
     }
+
     const enDiv = document.getElementById('enDiv');
     const esDiv = document.getElementById('esDiv');
 
     lanChange(); // Call it initially to set the correct language on page load
+
     // Get selected values
     element.addEventListener('change', function () {
+        console.log('Selected:', element.value);
         lanChange();
     });
-
 });
